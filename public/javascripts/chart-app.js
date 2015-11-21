ae9299f8aaea6597f25b9f219e9f4ed6a2ce9248
dani3ldnguy3n <danieldnguyen@yahoo.com>
2015-11-21 05:50:15 +0000

    initial commit

--- public/javascripts/chart-app.js
+++ public/javascripts/chart-app.js
@@ -1,3 +1,4 @@
+
 var app = angular.module('simple-chart', []);
 google.load("visualization", "1", {packages:["corechart"]});
 
@@ -7,10 +8,53 @@ app.controller('MainController', ['$scope', '$http',  function($scope, $http) {
   var dataArray = formatDataForView(data);
 
   var table = google.visualization.arrayToDataTable(dataArray, false);
-  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+  var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+
   
-  var options = {'title':'Company Sales'}
+  var options = {
+    title:'Sale Performance over Years ',
+    //subtitle: 'League of Legends World Championship vs Dota2 The International',
+    //chart: {title:'eSports Prize Pools',
+    //subtitle: 'League of Legends World Championship vs Dota2 The International '},
+    series:{
+      0:{color:'#000066'},
+      1:{color: '#990000'}
+    },
+
+    width: 900,
+    hAxis: {
+      title: 'Year',
+      titleTextStyle: {
+        
+        italic: false,
+        bold: true,
+      },
+      format: '',
+      gridlines: {count: 5},
+    },
+    
+    
+    
+    vAxis: {
+      title: 'In Billions',
+      titleTextStyle:{
+        bold: true,
+        italic: true,
+      },
+      textStyle: {
+        color: 'green'
+        
+      },
+     format: 'currency',
+      gridlines: {count: 11},
+      
+    }
+    
+      
+
+  };
   chart.draw(table, options);
+  
 
   });
 }]);
@@ -36,4 +80,5 @@ function formatDataForView(data) {
     });
   
     return dataArray;
+
 }
